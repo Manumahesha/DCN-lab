5543674368bd8fdfd15a74e3da2efaec19065180
Mahadevaprasad <129720075+Manumahesha@users.noreply.github.com>
2023-12-18 23:29:25 +0530

    Create leaky.java

---
+++ leaky.java
@@ -0,0 +1,51 @@
+import java.math.*;
+import java.util.*;
+import java.util.Random;
+import java.io.*;
+import java.lang.*;
+
+public class leaky {
+
+    public static void main(String args[]) 
+    {
+        int drop=0,mini,i,o_rate,b_size,nsec,p_remain=0;
+        int packet[]=new int[100];
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the bucket size:");
+        b_size= in.nextInt();
+        System.out.println("Enter output rate:");
+        o_rate=nextInt();
+        System.out.println("Enter the number of seconds to simulate:");
+        nsec=in.nextInt();
+        Random rand=new Random();
+        for (i=0;i<nsec;i++) {
+            packet[i]=(rand.nextInt(1000));
+        }
+        System.out.println("Seconds packet recieved packet sent  packets left  packets dropped:");
+        System.out.println("       ");
+        for (i=0;i<nsec;i++) 
+        {
+            p_remain+=packet[i];
+            if(p_remain>b_size){
+            drop=p_remain-b_size;
+            p_remain=b_size;
+            System.out.println(i+1+"          ");
+            System.out.println(packet[i]+"   ");
+            mini=math.min(p-remain,o_rate);
+            System.out.println(mini+"   ");
+            p_remain=p_remain-mini;
+            System.out.println(drop+"   ");
+            System.out.println("   ");
+            drop=0;
+        }
+    }
+    while(p_remain!=0){
+        if(p_remain>b_size){
+            drop=p_return-b_size;
+        }
+        mini=math.min(p-remain,o_rate);
+        System.out.println("   "+p_remain+"   "+drop);
+        drop=0;
+        }
+    }
+}    
