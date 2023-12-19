42d8cfead499b7611663e3fe3dd48766b4e096ac
Mahadevaprasad <129720075+Manumahesha@users.noreply.github.com>
2023-12-19 21:55:53 +0530

    leaky.java

--- leaky.java
+++ leaky.java
@@ -3,9 +3,7 @@ import java.util.*;
 import java.util.Random;
 import java.io.*;
 import java.lang.*;
-
 public class leaky {
-
     public static void main(String args[]) 
     {
         int drop=0,mini,i,o_rate,b_size,nsec,p_remain=0;
@@ -18,30 +16,32 @@ public class leaky {
         System.out.println("Enter the number of seconds to simulate:");
         nsec=in.nextInt();
         Random rand=new Random();
-        for (i=0;i<nsec;i++) {
-            packet[i]=(rand.nextInt(1000));
-        }
-        System.out.println("Seconds  Packet recieved  Packet sent  Packets left  Packets dropped:");
-        System.out.println("       ");
         for (i=0;i<nsec;i++) 
-        {
-            p_remain+=packet[i];
-            if(p_remain>b_size){
-            drop=p_remain-b_size;
-            p_remain=b_size;
-            System.out.println(i+1+"          ");
-            System.out.println(packet[i]+"   ");
-            mini=math.min(p-remain,o_rate);
-            System.out.println(mini+"   ");
-            p_remain=p_remain-mini;
-            System.out.println(drop+"   ");
-            System.out.println("   ");
-            drop=0;
-        }
-    }
-    while(p_remain!=0){
-        if(p_remain>b_size){
-            drop=p_return-b_size;
+            {
+                packet[i]=(rand.nextInt(1000));
+            }
+            System.out.println("Seconds  Packet recieved  Packet sent  Packets left  Packets dropped:");
+            System.out.println("       ");
+            for (i=0;i<nsec;i++) 
+            {
+                p_remain+=packet[i];
+                if(p_remain>b_size)
+                {
+                    drop=p_remain-b_size;
+                    p_remain=b_size;
+                    System.out.println(i+1+"          ");
+                    System.out.println(packet[i]+"   ");
+                    mini=math.min(p-remain,o_rate);
+                    System.out.println(mini+"   ");
+                    p_remain=p_remain-mini;
+                    System.out.println(drop+"   ");
+                    System.out.println("   ");
+                    drop=0;
+                }
+            }
+            while(p_remain!=0){
+                if(p_remain>b_size){
+                    drop=p_return-b_size;
         }
         mini=math.min(p-remain,o_rate);
         System.out.println("   "+p_remain+"   "+drop);
